c03f1938ad652a109e0e8478e5dd8d8ed389de71
Souvik011 <iamsouvik011@gmail.com>
2023-08-19 00:39:24 +0530

    Request Response handles with node JS

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ const http = require('http');
 // http.createServer(rqListener);
 
 const server = http.createServer((req,res) => {
-    console.log("Souvik Chakraborty");
+    let url = req.url;
+    res.setHeader('Content-Type' , 'text/html');
+    if(url == '/home'){
+        res.write('<head>');
+        res.write('<body><h1>Welcome Home </h1></body>');
+        res.write('</head>');
+        res.end();
+    }else if(url == '/about'){
+        res.write('<head>');
+        res.write('<body><h1>Welcome to About US page </h1></body>');
+        res.write('</head>');
+        res.end();
+    } else if(url == '/node'){
+        res.write('<head>');
+        res.write('<body><h1>Welcome to my Node JS Project </h1></body>');
+        res.write('</head>');
+        res.end();
+    }
 });
 
 server.listen(4000);
