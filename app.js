b47491f519e1bec39618c687dfb1ec37445146ac
Souvik011 <iamsouvik011@gmail.com>
2023-08-20 00:48:54 +0530

    Writing and reading from a file

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 const http = require('http');
-
-// function rqListener(req,res) {
-
-// };
-
-// http.createServer(rqListener);
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     let url = req.url;
-    res.setHeader('Content-Type' , 'text/html');
-    if(url == '/home'){
-        res.write('<head>');
-        res.write('<body><h1>Welcome Home </h1></body>');
-        res.write('</head>');
-        res.end();
-    }else if(url == '/about'){
-        res.write('<head>');
-        res.write('<body><h1>Welcome to About US page </h1></body>');
-        res.write('</head>');
-        res.end();
-    } else if(url == '/node'){
-        res.write('<head>');
-        res.write('<body><h1>Welcome to my Node JS Project </h1></body>');
-        res.write('</head>');
-        res.end();
+    const method = req.method;
+    if(url == '/'){
+        fs.readFile("message.txt",(err,data) => {
+            if(err){
+                console.log(err);
+            }
+            res.write('<html>');
+            res.write('<head><title>Enter Message</title><head>');
+            res.write(`<body>${data}</body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        });  
     }
+    if(url === '/message' && method === "POST"){
+        const body = [];
+        req.on("data" , (chunk) => {
+            body.push(chunk);
+        });
+        req.on("end",() => {
+            const parseBody = Buffer.concat(body).toString();
+            const msg = parseBody.split("=")[1];
+            fs.writeFileSync('message.txt',msg);
+        });
+        
+        res.statusCode=302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+   
+    
+
 });
 
 server.listen(4000);
