fb1e2988bd6526313400fbfcae0e8a714390f2db
Souvik011 <iamsouvik011@gmail.com>
2023-08-22 23:07:23 +0530

    Routers and Filters in Express JS

--- app.js
+++ app.js
@@ -5,29 +5,29 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./Routes/admin');
+const shopRoutes = require('./Routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use("/admin",adminRoutes);
+
+app.use("/shop",shopRoutes);
+
+app.get("/",(req,res,next) => {
+    res.send('<h1 >Hello from Express </h1>');
+});
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>There is Some ERROR IN LOADING</h1>');
+});
+
+app.listen(4000);
+
+
+
+
 // app.use((req,res,next) => {
 //     console.log('in the middleware');
 //     next();// this allows request to continue to the next middleware in line 
 // });
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/',(req,res,next)=>{
-    // console.log("this always continues");
-    next();
-});
-app.use('/add-product',(req,res,next) => {
-    // console.log('in the ADD PRODUCT');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">ADD PRODUCT</button></form>');
-});
-
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/',(req,res,next) => {
-    // console.log('in the middleware 2');
-    res.send('<h1 >Hello from Express </h1>');
-});
-
-app.listen(4000);
